8d6da3e03f1fe8af2042502590bdc71cda108afa
lRethi <azraethly@gmail.com>
2026-08-10 11:33:16 -0300

    0.6.0b
    Corrigida a lógica do STM para ter suporte de fato à usar um trimpot soldado em PA1 usando o ADC do STM, também atualizado o README.md para manter maior consistência com as novas alterações e adições.

--- firmware/stm32/leitorPurezaAr/Core/Src/main.c
+++ firmware/stm32/leitorPurezaAr/Core/Src/main.c
@@ -37,7 +37,7 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 extern USBD_HandleTypeDef hUsbDeviceFS;
-#define FILTER_SIZE 8
+#define FILTER_SIZE 4
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -46,6 +46,7 @@ extern USBD_HandleTypeDef hUsbDeviceFS;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
 
 /* USER CODE BEGIN PV */
 uint16_t varPotValue;
@@ -56,6 +57,7 @@ uint8_t indice = 0;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -78,15 +80,28 @@ uint8_t CDC_Transmit(uint8_t *buffer, uint16_t length) {
 }
 
 uint16_t MediaMovel(uint16_t novaLeitura) {
+	bool inicializado = false;
 	uint32_t soma = 0;
 
-	amostras[indice] = novaLeitura;
-	indice = (indice + 1) % FILTER_SIZE;
+	if (!inicializado) {
+		for (int i = 0; i < FILTER_SIZE; i++) {
+			amostras[i] = novaLeitura;
+		}
 
-	for (int i = 0; i < FILTER_SIZE; i++)
+		inicializado = true;
+	} else {
+		amostras[indice] = novaLeitura;
+		indice = (indice + 1) % FILTER_SIZE;
+	}
+
+	for (int i = 0; i < FILTER_SIZE; i++) {
 		soma += amostras[i];
+	}
 
 	return soma / FILTER_SIZE;
+}
+uint16_t ConverterConcentracao(uint16_t adc) {
+	return ((uint32_t) adc * 1000) / 4095;
 }
 /* USER CODE END 0 */
 
@@ -118,6 +133,7 @@ int main(void) {
 	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
 	MX_USB_DEVICE_Init();
+	MX_ADC1_Init();
 	/* USER CODE BEGIN 2 */
 	HAL_Delay(10000);
 	/* USER CODE END 2 */
@@ -125,21 +141,32 @@ int main(void) {
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	while (1) {
-		short adc = 1024;
+		HAL_ADC_Start(&hadc1);
+		HAL_ADC_PollForConversion(&hadc1, 100);
+
+		uint16_t adc = HAL_ADC_GetValue(&hadc1);
+
+		HAL_ADC_Stop(&hadc1);
+
+		uint16_t concentracao = ConverterConcentracao(adc);
+
 		bool filtroAtivo = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)
 				== GPIO_PIN_RESET;
+
 		uint16_t valor;
 
 		if (filtroAtivo) {
-			valor = MediaMovel(adc);
+			valor = MediaMovel(concentracao);
 		} else {
-			valor = adc;
+			valor = concentracao;
 		}
+
 		char buffer[32];
 
 		sprintf(buffer, "%u;%u\r\n", valor, filtroAtivo ? 1 : 0);
 
 		CDC_Transmit((uint8_t*) buffer, strlen(buffer));
+
 		HAL_Delay(5000);
 		/* USER CODE END WHILE */
 
@@ -182,11 +209,54 @@ void SystemClock_Config(void) {
 	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
 		Error_Handler();
 	}
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC | RCC_PERIPHCLK_USB;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
 	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
 	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
 		Error_Handler();
 	}
+}
+
+/**
+ * @brief ADC1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_ADC1_Init(void) {
+
+	/* USER CODE BEGIN ADC1_Init 0 */
+
+	/* USER CODE END ADC1_Init 0 */
+
+	ADC_ChannelConfTypeDef sConfig = { 0 };
+
+	/* USER CODE BEGIN ADC1_Init 1 */
+
+	/* USER CODE END ADC1_Init 1 */
+	/** Common config
+	 */
+	hadc1.Instance = ADC1;
+	hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+	hadc1.Init.ContinuousConvMode = DISABLE;
+	hadc1.Init.DiscontinuousConvMode = DISABLE;
+	hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	hadc1.Init.NbrOfConversion = 1;
+	if (HAL_ADC_Init(&hadc1) != HAL_OK) {
+		Error_Handler();
+	}
+	/** Configure Regular Channel
+	 */
+	sConfig.Channel = ADC_CHANNEL_1;
+	sConfig.Rank = ADC_REGULAR_RANK_1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN ADC1_Init 2 */
+
+	/* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
